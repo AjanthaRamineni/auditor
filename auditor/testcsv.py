3d24e4762d0088fe5baeebab149fe6d3eade2323
Harshita Vuradi <hvuradi@Hashita-MacBook-Pro.local>
2017-04-11 08:50:27 -0400

    tests do_add_headers feature of auditor

--- auditor/testcsv.py
+++ auditor/testcsv.py
@@ -1,19 +1,27 @@
+#! /Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 import csv
 import unittest
 from docopt import docopt
-import yaml
+import sys
+import auditor
 
+import yaml
+#from auditor.mappings import Mappings
+_file = 'Workbook1.csv'
+_config = 'auditor.example.conf.yaml'
+#_output = '--output'
+import pdb
 
 class Test(unittest.TestCase):
-	def test_do_add_headers(self,args=docopt(docstr)):
-
-  		with open(args[_config], 'r') as config_file:
-  			global config
-  			config = yaml.load(config_file.read())
-  		csv_file = open(args[_file], 'r')
-  		data = csv.reader(csv_file, **config['csv_conf'])
-  		data1 = do_add_headers(data, config.get('new_headers'))
-  		print(data1)
+	def test_do_add_headers(self):
+		print('test')
+		with open(_config, 'r') as config_file:
+			global config
+			config = yaml.load(config_file.read())
+		csv_file = open(_file, 'r')
+		data = csv.reader(csv_file, **config['csv_conf'])
+		data1 = auditor.do_add_headers(data, config.get('new_headers'))
+		print(data1)
     
 
 	#def test_do_add_headers(self):
@@ -28,6 +36,7 @@ class Test(unittest.TestCase):
 	#		print(r)
 	#		print(r1)
 	#	self.assertListEqual(r,r1)
+
 unittest.main()
 
 
